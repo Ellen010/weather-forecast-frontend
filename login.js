267b37dc79e443021cc055d9da77ff6805b11220
Elena <ewlen04@gmail.com>
2025-02-28 14:59:28 +0100

    bugs corrections3

--- login.js
+++ login.js
@@ -5,7 +5,7 @@ document.querySelector("#register").addEventListener("click", function (){
         email:document.querySelector("#registerEmail").value,
         password:document.querySelector("#registerPassword").value,
     };
-    fetch ("https://weather-forecast-backend-eta.vercel.app/users/singup", {
+    fetch ("https://weather-forecast-backend-eta.vercel.app/users/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json"},
         body: JSON.stringify (user),
